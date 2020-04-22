6fc7702d755c9ab68337efe07cfd0a3a01aa7876
Abhijeet.Mohanty <mabhijeet1995@gmail.com>
2020-04-21 20:06:43 -0500

    Added @PostConstruct and @PreDestroy

--- src/main/java/com/spring/tutorial/annotations/entity/BasketballCoach.java
+++ src/main/java/com/spring/tutorial/annotations/entity/BasketballCoach.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class BasketballCoach implements ICoach {
 
@@ -11,6 +14,12 @@ public class BasketballCoach implements ICoach {
     @Qualifier(value = "sadFortuneService")
     @Autowired
     private IFortuneService fortuneService;
+
+    // Method invoked only after the bean is created along with its injections
+    @PostConstruct
+    public void doSomethingPostConstruct() {
+        System.out.println("This method runs after constructing the bean");
+    }
 
     @Override
     public String getDailyWorkout() {
@@ -20,5 +29,11 @@ public class BasketballCoach implements ICoach {
     @Override
     public String getFortune() {
         return fortuneService.getFortune();
+    }
+
+    // Method invoked only after the bean is destroyed
+    @PreDestroy
+    public void doSomethingPreDestroy() {
+        System.out.println("This method runs after destruction of the bean");
     }
 }
